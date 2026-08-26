47c64d8e7137872cc2f75633ba3919807697f493
RicBKewL <ricbkewl@gmail.com>
2026-08-26 16:36:12 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='agape-golf-v8';
+const CACHE_NAME='agape-golf-v9';
 const APP_SHELL=['./','./index.html','./app.js','./styles.css','./manifest.webmanifest','./agape-golf-logo.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
