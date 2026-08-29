2abfd1dfbc081fb5255d5be20edc2c035b52c7a8
RicBKewL <ricbkewl@gmail.com>
2026-08-28 23:59:41 -0700

    Bump app cache to version 65

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='agape-golf-v64';
+const CACHE_NAME='agape-golf-v65';
 const APP_SHELL=['./','./index.html','./app.js','./styles.css','./manifest.webmanifest','./agape-golf-logo.png','./rick-kulon-profile.jpg','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
