72c01a267a3c9fe46237bf91e331135c7c9c4fd4
RicBKewL <ricbkewl@gmail.com>
2026-08-28 22:42:55 -0700

    Refresh app cache for weather version 56

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='agape-golf-v55';
+const CACHE_NAME='agape-golf-v56';
 const APP_SHELL=['./','./index.html','./app.js','./styles.css','./manifest.webmanifest','./agape-golf-logo.png','./rick-kulon-profile.jpg','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
