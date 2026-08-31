a1d8c1b1e97e3bd7b1dd2581fb5cabc0b2eb48b9
RicBKewL <ricbkewl@gmail.com>
2026-08-30 19:13:35 -0700

    Refresh offline cache for version 92

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='agape-golf-v91';
+const CACHE_NAME='agape-golf-v92';
 const APP_SHELL=['./','./index.html','./app.js','./styles.css','./manifest.webmanifest','./agape-golf-logo.png','./rick-kulon-profile.jpg','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
