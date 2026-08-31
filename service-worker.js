e2c41e420aef46fbd883b874b267e26adf63efae
RicBKewL <ricbkewl@gmail.com>
2026-08-30 20:10:29 -0700

    Version 95: fix catalog rounds and navigation order

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='agape-golf-v94';
+const CACHE_NAME='agape-golf-v95';
 const APP_SHELL=['./','./index.html','./app.js','./styles.css','./manifest.webmanifest','./agape-golf-logo.png','./rick-kulon-profile.jpg','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
