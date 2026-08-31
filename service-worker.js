f02669d5ae4969d877d48a98a86eaad9df53292a
RicBKewL <ricbkewl@gmail.com>
2026-08-31 07:10:03 -0700

    Refresh version 104 cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='agape-golf-v103';
+const CACHE_NAME='agape-golf-v104';
 const APP_SHELL=['./','./index.html','./guide-i18n.js','./app.js','./styles.css','./manifest.webmanifest','./agape-golf-logo.png','./rick-kulon-profile.jpg','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
